71e6b4a0e56521b8ce127aba6728cd2ae36bf3e7
oladayo ahmod <oladayoahmod112@gmail.com>
2023-01-25 20:49:01 -0800

    pages navbar styles

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
 // import '@/styles/globals.css'
+// import '@/styles/plugins/feature.css'
+// import '@/styles/plugins/jquery-ui.min.css'
+import '@/styles/vendor/nice-select.css'
+import '@/styles/vendor/odometer.css'
+import '@/styles/style.css'
+
+
 import { AppProps } from 'next/app'
 import Layout from '../components/layouts/index'
 export default function MyApp({ Component, pageProps }) {

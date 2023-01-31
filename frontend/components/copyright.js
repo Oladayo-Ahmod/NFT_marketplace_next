274f03faf0de4ba0b51e418e9aefb7dd3b546f1a
oladayo ahmod <oladayoahmod112@gmail.com>
2023-01-31 02:22:46 -0800

    copyright

---
+++ frontend/components/copyright.js
@@ -0,0 +1,34 @@
+const CopyRight =()=>{
+    return (
+        <>
+             <div className="copy-right-one ptb--20 bg-color--1">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-lg-6 col-md-12 col-sm-12">
+                            <div className="copyright-left">
+                                <span>©2023 NFT MARKETPLACE, Inc. All rights reserved.</span>
+                                <ul className="privacy">
+                                    <li><a href="terms-condition.html">Terms</a></li>
+                                    <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-sm-12">
+                            <div className="copyright-right">
+                                <ul className="social-copyright">
+                                    <li><a href="#"><i></i></a></li>
+                                    <li><a href="#"><i ></i></a></li>
+                                    <li><a href="#"><i ></i></a></li>
+                                    <li><a href="#"><i></i></a></li>
+                                    <li><a href="#"><i></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CopyRight
--- frontend/components/layouts/index.js
+++ frontend/components/layouts/index.js
@@ -1,11 +1,13 @@
 import Navbar from '../Navbar'
 import Footer from '../footer'
+import CopyRight from '../copyright'
 const Layout =({children})=>{
     return (
         <div className='content'>
             <Navbar />
             {children}
             <Footer />
+            <CopyRight />
         </div>
     )
 }

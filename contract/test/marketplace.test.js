2f36184a19a2d25ce76346941cda3f080addcd24
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-09 14:29:48 -0800

    test listing price update

--- contract/test/marketplace.test.js
+++ contract/test/marketplace.test.js
@@ -1,15 +1,16 @@
 const {ethers, network} = require('hardhat')
 const {assert,expect} = require('chai')
 describe("NFT Marketplace", function (){
-    let provider, deploy
+    let provider, marketplace
     beforeEach(async ()=>{
         provider = await ethers.getContractFactory("Marketplace")
-        deploy = await provider.deploy()
+        marketplace = await provider.deploy()
 
     })
 
     it("deployed contract", async function(){
-        const address = await deploy.deployed()
+        const address = await marketplace.deployed()
+        console.log(marketplace.address);
         assert.notEqual(address,0x0)
         assert.notEqual(address,'')
         assert.notEqual(address,undefined)
@@ -17,8 +18,24 @@ describe("NFT Marketplace", function (){
 
     })
     it("has a valid constructor", async ()=>{
-        const name = await deploy.name()
-        console.log(name,'name');
+        const name = await marketplace.name()
         assert.equal(name,"Ahmad Token")
     })
+
+    describe("pricing", function (){
+        it("has a correct listing price", async ()=>{
+            const listingPrice = await marketplace.getListingPrice()
+            assert.equal(listingPrice.toString(),ethers.utils.parseEther("0.05"))
+        })
+        it("authorized price update", async ()=>{
+            const listingPrice = await marketplace.getListingPrice()
+            const updatedPrice = ethers.utils.parseEther("0.08")
+            await marketplace.connect(marketplace.address)
+            await marketplace.updateListingPrice(updatedPrice)
+            const afterPriceUpdate = await marketplace.getListingPrice()
+            assert.equal(afterPriceUpdate,updatedPrice)
+        })
+    })
+   
+
 })

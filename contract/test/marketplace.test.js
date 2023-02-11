594e631c02321c79d360dfc304feddee0f8a5fde
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-11 02:23:30 -0800

    test sales making

--- contract/test/marketplace.test.js
+++ contract/test/marketplace.test.js
@@ -24,7 +24,7 @@ describe("NFT Marketplace", function (){
     it("creates token", async ()=>{
         const listingPrice = await marketplace.getListingPrice()
         const itemPrice = ethers.utils.parseEther('1')
-         await marketplace.createToken("https://token.com",itemPrice,{value :listingPrice.toString()})
+        await marketplace.createToken("https://token.com",itemPrice,{value :listingPrice.toString()})
       
     })
 
@@ -34,12 +34,21 @@ describe("NFT Marketplace", function (){
             assert.equal(listingPrice.toString(),ethers.utils.parseEther("0.05"))
         })
         it("authorized price update", async ()=>{
-            const listingPrice = await marketplace.getListingPrice()
             const updatedPrice = ethers.utils.parseEther("0.08")
             await marketplace.connect(marketplace.address)
             await marketplace.updateListingPrice(updatedPrice)
             const afterPriceUpdate = await marketplace.getListingPrice()
             assert.equal(afterPriceUpdate.toString(),updatedPrice)
+        })
+    })
+
+    describe("items sales", ()=>{
+        it("creates sales",async ()=>{
+            const listingPrice = await marketplace.getListingPrice()
+            const itemPrice = ethers.utils.parseEther('1')
+            await marketplace.createToken("https://token.com",itemPrice,{value :listingPrice.toString()})
+            const sale = await marketplace.createSale(1,{value :itemPrice})
+            console.log(sale);
         })
     })
    

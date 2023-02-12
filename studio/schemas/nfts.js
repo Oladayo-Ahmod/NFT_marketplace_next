9a9b8e3d000a7eb0800f3534f97a16b13d1f2334
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-11 18:58:57 -0800

    set up table schemas

---
+++ studio/schemas/nfts.js
@@ -0,0 +1,46 @@
+export const NFTS = {
+    name : 'nfts',
+    title : 'NFTs',
+    type : 'document',
+    fields : [
+        {
+            name : 'hash',
+            title : 'Transaction Hash',
+            type : 'string'
+        },
+        {
+            name : 'seller',
+            title : 'NFT Seller',
+            type : 'string'
+        },
+        {
+            name : 'owner',
+            title : 'NFT Owner',
+            type : 'string'
+        },
+        {
+            name : 'price',
+            title : 'NFT Price',
+            type : 'number'
+        },
+        {
+            name : 'sold',
+            title : 'Sold',
+            type : 'boolean'
+        },
+        {
+            name : 'timestamp',
+            title : 'Timestamp',
+            type : 'datetime'
+        },
+        {
+            name : 'image',
+            title : 'NFT Image',
+            type : 'image'
+        }
+    ],
+    preview: {
+        select: {title: 'NFT Image', media: 'image'},
+      },
+
+}

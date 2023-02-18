252fcf9f8dd6eac6e49cd5dec122007a8c104ef9
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-18 06:59:15 -0800

    connecting wallet

--- frontend/context/TransactionContext.js
+++ frontend/context/TransactionContext.js
@@ -13,9 +13,8 @@ const TransactionProviderr =({children})=>{
 const [account,setAccount] = useState()
 const test = 'hello'
 const connectWallet = async function(metamask = connector){
-    console.log('confirm');
     if (metamask) {
-        const accounts = await metamask.request({method : 'eth_requestAccount'}) 
+        const accounts = await connector.request({method : 'eth_requestAccounts'}) 
         setAccount(accounts[0])
     }
     else{

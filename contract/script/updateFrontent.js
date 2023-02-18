d3dbaa513d770753833599b50af1cbb249ad4d22
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-18 09:17:49 -0800

    dynamic frontend constants update

---
+++ contract/script/updateFrontent.js
@@ -0,0 +1,12 @@
+const {ethers} = require('hardhat')
+const fs = require('fs')
+require('dotenv').config()
+
+module.exports = function(){
+    if (process.env.UPDATEFRONTEND) {
+        
+    }
+}
+
+const ABI =
+const ADDRESS

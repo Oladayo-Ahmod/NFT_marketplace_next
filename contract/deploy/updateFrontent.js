f16bf4399422511c235777d1d2d427094b021dd6
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-18 22:40:25 -0800

    dynamic frontend constants update

--- contract/deploy/updateFrontent.js
+++ contract/deploy/updateFrontent.js
@@ -8,10 +8,15 @@ module.exports = async function(){
         const ABI = '../frontend/constants/abi.json'  // abi location
         const ADDRESS = '../frontend/constants/address.json' // contract address location
         
-            const transaction = await hre.ethers.getContractFactory('Marketplace')
-            const deploy = await transaction.deploy()
-            console.log('---------------updating---contract-----------address');
-            fs.writeFileSync(ADDRESS,JSON.parse(deploy.address))
-            console.log('--------------updated--------contract------------address');
+        const transaction = await hre.ethers.getContractFactory('Marketplace')
+        const deploy = await transaction.deploy()
+        console.log('---------------updating---contract-----------address');
+        fs.writeFileSync(ADDRESS,JSON.stringify(deploy.address))
+        console.log('--------------updated--------contract------------address');
+        const abi = transaction.interface.format(ethers.utils.FormatTypes.json)
+        console.log('----------updating----abi---')
+        fs.writeFileSync(ABI,abi)
+        console.log('----------updated----abi---')
+
     }
 }

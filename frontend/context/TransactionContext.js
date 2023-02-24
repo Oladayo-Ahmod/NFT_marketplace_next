8597edadac096f766ea9574a85c3a5c75634a92d
oladayo ahmod <oladayoahmod112@gmail.com>
2023-02-24 06:20:12 -0800

    create nft item functionality

--- frontend/context/TransactionContext.js
+++ frontend/context/TransactionContext.js
@@ -1,6 +1,8 @@
 import React, { Context,createContext, useEffect, useState } from "react"
 import {abi,address} from '../constants/index'
 import {ethers} from 'ethers'
+import Swal from "sweetalert2"
+
 
 const TransactionContext = React.createContext()
 
@@ -15,6 +17,7 @@ const TransactionProviderr =({children})=>{
     const [formInput,setFormInput] = useState()
     const [account,setAccount] = useState()
 
+
     const connectWallet = async function(metamask = connector){
         if (metamask) {
             const accounts = await connector.request({method : 'eth_requestAccounts'})
@@ -25,10 +28,36 @@ const TransactionProviderr =({children})=>{
         }
     }
 
+    useEffect(()=>{
+        connectWallet(connector)
+    },[account])
+
     const CreateNft = async function(metamask = connector){
-        const provider = new ethers.providers.Web3Provider(metamask);
-        const signer = provider.getSigner()
-        const contract = new ethers.Contract(abi,signer)
+        try{
+            const provider = new ethers.providers.Web3Provider(connector);
+            const signer = provider.getSigner()
+            const contract = new ethers.Contract(address,abi,signer)
+            const price = new ethers.utils.parseEther('1','ether')
+            const url = 'https://mytoken.com'
+            const listingPrice = await contract.getListingPrice()
+            const transaction = await contract.createToken(url, price, { value: listingPrice })
+            await transaction.wait()
+            Swal({
+                position: 'top-end',
+                icon: 'success',
+                text: `You have successfully purchased this item at ${price} ETH`,
+                showConfirmButton: false,
+                timer: 2500
+            })
+
+            
+        }
+        catch(error){
+            console.log(error)
+        }
+       
+       
+
     }
 
     return (
